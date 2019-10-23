01e44e89f2bbebc12f447d0b1549ee5d56d7d439
shen <davidshen@asana.com>
2019-10-23 21:59:17 +0000

    added a click handler, appended input to page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+$("button").click(function(){
+    var ask = $("input").val();
+    $(".output").append(ask);
+});
 
 
-  
-  
-  
-  
+
+
+
+
 });
